dccf3fef27fb77fb74a6faafe6c8925a3581211e
hudulgi <jeonghu@gmail.com>
2020-11-08 12:59:53 +0900

    초기

---
+++ coinking_base.py
@@ -0,0 +1,37 @@
+import time
+import pybithumb
+import datetime
+
+
+def get_target_price(ticker):
+    df = pybithumb.get_candlestick(ticker)
+    yesterday = df.iloc[-2]
+
+    #today_open = yesterday['close']
+    #yesterday_high = yesterday['high']
+    #yesterday_low = yesterday['low']
+    #target = today_open + (yesterday_high - yesterday_low) * 0.5
+    #return target
+    return []
+
+
+def get_target_db(ticker):
+    df = pybithumb.get_candlestick(ticker)
+    df.to_csv(f"{ticker}.csv")
+
+
+now = datetime.datetime.now()
+mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+target_price = get_target_price("BTC")
+
+while True:
+    now = datetime.datetime.now()
+    if mid < now < mid + datetime.delta(seconds=10):
+        #target_price = get_target_price("BTC")
+        get_target_db("BTC")
+        mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+
+    current_price = pybithumb.get_current_price("BTC")
+    print(current_price)
+
+    time.sleep(1)
