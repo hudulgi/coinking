5a250780f62cec1151fad719d500fd3f8455a653
hudulgi <jeonghu@gmail.com>
2020-11-24 12:52:57 +0900

    단위에 따라 정수형과 실수형으로 출력되도록 반영

--- pybithumb_trade.py
+++ pybithumb_trade.py
@@ -1,5 +1,6 @@
 import pybithumb
 import mykey
+import sys
 
 con_key = mykey.con_key
 sec_key = mykey.sec_key
@@ -10,50 +11,58 @@ bithumb = pybithumb.Bithumb(con_key, sec_key)
 def price_filter(value):
     a = 1
     coff = 10000
-    value *= 10000
-    if value < 1 * coff:
+    if value < 1:
         a = 0.0001 * coff
-    if 1 * coff <= value < 10 * coff:
+        return value * coff // a * a / coff
+    if 1 <= value < 10:
         a = 0.001 * coff
-    if 10 * coff <= value < 100 * coff:
+        return value * coff // a * a / coff
+    if 10 <= value < 100:
         a = 0.01 * coff
-    if 100 * coff <= value < 1000 * coff:
+        return value * coff // a * a / coff
+    if 100 <= value < 1000:
         a = 0.1 * coff
-    if 1000 * coff <= value < 5000 * coff:
-        a = 1 * coff
-    if 5000 * coff <= value < 10000 * coff:
-        a = 5 * coff
-    if 10000 * coff <= value < 50000 * coff:
-        a = 10 * coff
-    if 50000 * coff <= value < 100000 * coff:
-        a = 50 * coff
-    if 100000 * coff <= value < 500000 * coff:
-        a = 100 * coff
-    if 500000 * coff <= value < 1000000 * coff:
-        a = 500 * coff
-    if value > 1000000 * coff:
-        a = 1000 * coff
+        return value * coff // a * a / coff
+    if 1000 <= value < 5000:
+        a = 1
+    if 5000 <= value < 10000:
+        a = 5
+    if 10000 <= value < 50000:
+        a = 10
+    if 50000 <= value < 100000:
+        a = 50
+    if 100000 <= value < 500000:
+        a = 100
+    if 500000 <= value < 1000000:
+        a = 500
+    if 1000000 <= value:
+        a = 1000
 
-    return value // a * a / coff
+    return int(value // a * a)
 
 
 def amount_filter(price, ref):
     a = 1
     coff = 10000
-    _amount = ref / price * coff
+    _amount = ref / price
     if price < 100:
-        a = 10 * coff
+        a = 10
     if 100 <= price < 1000:
-        a = 1 * coff
+        a = 1
     if 1000 <= price < 10000:
         a = 0.1 * coff
+        return _amount * coff // a * a / coff
     if 10000 <= price < 100000:
         a = 0.01 * coff
+        return _amount * coff // a * a / coff
     if 100000 <= price < 1000000:
         a = 0.001 * coff
+        return _amount * coff // a * a / coff
     if 1000000 <= price:
         a = 0.0001 * coff
-    return _amount // a * a / coff
+        return _amount * coff // a * a / coff
+
+    return int(_amount // a * a)
 
 
 def sell_crypto_currency(ticker):
@@ -65,9 +74,7 @@ def sell_crypto_currency(ticker):
 
 
 if __name__ == '__main__':
-    aa = price_filter(20717000)
+    args = sys.argv[1:]
+    aa = price_filter(float(args[0]))
     print(aa)
-    print(amount_filter(aa, 3000))
-    print(bithumb.get_balance("XRP")[2])
-    order = bithumb.buy_limit_order("XRP", 300, 2.0)
-    print(order)
+    print(amount_filter(aa, float(args[1])))
